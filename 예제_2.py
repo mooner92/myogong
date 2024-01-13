fed307a8986f4584904690af015915472afbcf92
Mooner92 <54096383+mooner92@users.noreply.github.com>
2024-01-13 19:46:31 +0900

    add

---
+++ 예제_2.py
@@ -0,0 +1,11 @@
+my_tuple = (1, 2, 3)
+
+# indexing
+print(my_tuple[0])  # 1
+print(my_tuple[1])  # 2
+print(my_tuple[2])  # 3
+
+# slicing
+print(my_tuple[1:])  # (2,3)
+print(my_tuple[:2])  # (1,2)
+print(my_tuple[1:2])  # (2,)
