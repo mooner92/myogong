4aec0cb40405d8b0ebec002f665aeb9bbfce199f
Mooner92 <54096383+mooner92@users.noreply.github.com>
2024-01-13 19:57:32 +0900

    add

---
+++ 예제_3.py
@@ -0,0 +1,9 @@
+lambda x, y: x + y  # x+y를 진행하는 람다식
+
+add = lambda x, y: x + y  # 람다식은 변수로 참조 가능
+print(add(5, 4))  # 9
+
+num = [1, 2, 3, 4, 5]
+squares = list(map(lambda x: x**2, num))
+print(squares)  # [1,4,9,16,25]
+# num 배열의 각 원소를 제곱한 원소로 배열을 수정함
