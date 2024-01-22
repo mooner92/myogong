7322c084b9042525aab1231da9073a0c6a4f8fb3
Mooner92 <54096383+mooner92@users.noreply.github.com>
2024-01-23 01:41:52 +0900

    add

---
+++ p5.py
@@ -0,0 +1,9 @@
+def solution(arr1, arr2):
+    r1, c1 = len(arr1), len(arr1[0])
+    r2, c2 = len(arr2), len(arr2[0])
+    ret = [[0] * c2 for _ in range(r1)]  # a_b x c_d b=c일 때 a x d
+    for i in range(r1):
+        for j in range(c2):
+            for k in range(c1):
+                ret[i][j] += arr1[i][k] * arr2[k][j]
+    return ret
