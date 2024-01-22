a11b6dee36281ab08638c1abc174cde70458219f
Mooner92 <54096383+mooner92@users.noreply.github.com>
2024-01-23 04:24:42 +0900

    add

---
+++ p9.py
@@ -0,0 +1,10 @@
+def solution(demical):
+    stack = []
+    while demical > 0:
+        remainder = demical % 2
+        stack.append(str(remainder))
+        demical //= 2
+    binary = ""
+    while stack:
+        binary += stack.pop()
+    return binary
